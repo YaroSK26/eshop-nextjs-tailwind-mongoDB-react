4b6206e7bedbe9810e79ae6df13fcdf0180aac57
Yaro <jaroba0@gmail.com>
2023-06-10 18:39:20 +0200

    opravene vsetko..diki forefront.ai , access madar

--- nenavidim-next/components/ProductForm.js
+++ nenavidim-next/components/ProductForm.js
@@ -1,6 +1,3 @@
-/* eslint-disable react/jsx-key */
-/* adding product, updating new product*/
-
 import { useState } from "react";
 import axios from "axios";
 import { useRouter } from "next/router";
@@ -53,10 +50,10 @@ export default function ProductForm({_id,title:existingTitle,description:existin
         }
     }
 
-      function  updatedImagesOrder(){
-        console.log(images)
-        setImages(images)
-      }
+    function updatedImagesOrder(newImagesOrder) {
+        setImages(newImagesOrder);
+    }
+
     return (
    
         <form onSubmit={saveProduct} action="">
@@ -64,26 +61,28 @@ export default function ProductForm({_id,title:existingTitle,description:existin
         <label htmlFor="1">Product name</label>
         <input id="1" type="text" placeholder="product name"  value={title} onChange={ev => setTitle (ev.target.value)}/>
         <label htmlFor="">Photos</label>
-        <div className="mb-2 flex flex-wrap gap-1">
-            <ReactSortable className="flex flex-wrap gap-1" list={images} setList={updatedImagesOrder}>
-            {!!images?.length && images.map(link => (
-                <div key={key} className=" h-24">
-                    <image src={link} alt="" className="rounded-lg" />
-                </div>
+       <div className="mb-2 flex flex-wrap gap-1">
+        <ReactSortable className="flex flex-wrap gap-1" list={images} setList={updatedImagesOrder} swapThreshold={1}>
+            {!!images?.length && images.map((link, index) => (
+            <div key={index} className=" h-24">
+                <img src={link} alt="" className="rounded-lg" />
+            </div>
             ))}
-             </ReactSortable>
-            {isUploding && (
-                <div className="h-24  p-1 flex items-center ">
-                        <Spinner/>
-                </div>
-            )}
-            <label className="cursor-pointer text-gray-500 rounded-lg bg-gray-200 gap-1 w-24 h-24  flex items-center justify-center"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+        </ReactSortable>
+        {isUploding && (
+            <div className="h-24  p-1 flex items-center ">
+            <Spinner/>
+            </div>
+        )}
+        <label className="cursor-pointer text-gray-500 rounded-lg bg-gray-200 gap-1 w-24 h-24  flex items-center justify-center">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
             </svg>
-                <div>Upload</div>
-                <input  type="file" className="hidden " onChange={uploadImages} />
-            </label>
+            <div>Upload</div>
+            <input  type="file" className="hidden " onChange={uploadImages} />
+        </label>
         </div>
+
          <label htmlFor="2">Description</label>
         <textarea id="2" placeholder="description" value={description} onChange={ev => setDescription (ev.target.value)}></textarea>
          <label htmlFor="3">Price (in USD)</label>
--- nenavidim-next/pages/api/upload.js
+++ nenavidim-next/pages/api/upload.js
@@ -1,49 +1,55 @@
-/* aby neparsov data json cez next js , pouzivame multiparty */
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import fs from 'fs';
+import mime from 'mime-types';
+import multiparty from 'multiparty';
 
-import multiparty from "multiparty"
-import {PutObjectCommand, S3Client} from "@aws-sdk/client-s3"
-import fs from "fs"
-import mime from "mime-types"
-const bucketName = "jaro-next-ecommerce"
+import { mongooseConnect } from "../../lib/mongoose";
+import { isAdminRequest } from "../../pages/api/auth/[...nextauth]";
 
+const bucketName = 'jaro-nextjs-ecommerce';
 
-export default async function handle(req,res){
-            const {fields,files} = await new Promise((resolve,reject) =>{
-            const form = new multiparty.Form()
-            form.parse(req,async (err,fields,files) =>{
-            if (err) reject (err)
-            resolve(files,fields)       
-    })
-    })
-    console.log("length:", files.file.length)
+export default async function handle(req, res) {
+  await mongooseConnect();
+  await isAdminRequest(req, res);
+  console.log('isAdminRequest called successfully');
 
+  try {
+    const form = new multiparty.Form();
+    const { fields, files } = await new Promise((resolve, reject) => {
+      form.parse(req, (err, fields, files) => {
+        if (err) reject(err);
+        resolve({ fields, files });
+      });
+    });
+    console.log('length:', files.file.length);
     const client = new S3Client({
-    region: "us-east-1",
-    credentials:{
-        accessKey: "proccess.env.S3_ACCESS_KEY",
-        secretAccessKey: "proccess.env.S3_SECRET_ACCESS_KEY",
-
-        },
-    })
-    const links = []
-    for (const file of files.file){
-        const ext = file.originalFilename.split(".").pop()
-        const newFilename = Date.now() + "." + ext
-        await  client.send(new PutObjectCommand({
+      region: 'eu-north-1',
+      credentials: {
+        accessKeyId: process.env.S3_ACCESS_KEY,
+        secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+      },
+    });
+    const links = [];
+    for (const file of files.file) {
+      const ext = file.originalFilename.split('.').pop();
+      const newFilename = Date.now() + '.' + ext;
+      await client.send(new PutObjectCommand({
         Bucket: bucketName,
         Key: newFilename,
         Body: fs.readFileSync(file.path),
-        ACL: "public-read",
+        ACL: 'public-read',
         ContentType: mime.lookup(file.path),
-    }));
-    const link = `https://${bucketName}.s3.amazonaws.com/${newFilename}`
-    links.push(link)
+      }));
+      const link = `https://${bucketName}.s3.amazonaws.com/${newFilename}`;
+      links.push(link);
     }
-
-   
-         return   res.json({links})
+    return res.json({links});
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Internal Server Error' });
+  }
 }
 
 export const config = {
-    api: {bodyParser: false},
-}
+  api: {bodyParser: false},
+};
